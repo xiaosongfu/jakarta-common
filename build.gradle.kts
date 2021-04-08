76f92e8a4f4857829d7f53e6d22510afc1a33784
semi.fu <semi.fu@xiaoi.com>
2021-04-08 13:24:30 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.jetbrains.kotlin.jvm") version "1.4.31"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.4.31"
-    "maven"
+//    id("org.springframework.boot") version "2.4.4"
+//    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jetbrains.kotlin.jvm") version "1.4.32"
+//    id("org.jetbrains.kotlin.plugin.spring") version "1.4.32"
+}
+
+apply {
+    plugin("maven")
 }
 
 group = "com.github.xiaosongfu"
@@ -17,11 +20,11 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework:spring-web:5.1.5.RELEASE")
 //	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
